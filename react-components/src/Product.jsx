56002a59b916d073890a81e9d986020e95734607
srini1022 <srinidhidevraj2267@gmail.com>
2026-03-28 12:48:04 +0530

    Initaia commit of Components and exprot and import JSX

---
+++ react-components/src/Product.jsx
@@ -0,0 +1,12 @@
+import "./Product.css";
+
+function Product(){
+    return (
+        <div className="Product">
+            <h3>Product title</h3>
+            <h5>PRoduct Decrption</h5>
+        </div>
+    )
+}
+
+export default Product;
---
+++ react-components/src/Producttab.jsx
@@ -0,0 +1,15 @@
+
+import Product from "./Product.jsx";
+
+function ProductTab(){
+    return (
+        <div>
+            <h2>Product Tab</h2>
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    )
+}
+
+export default ProductTab;  
